397931c4c1287f4a65341ec26c981641886ff357
athena <none>
2001-07-13 08:12:19 -0400

    [project @ 2001-07-13 12:12:19 by athena]
    Started working on dsp/
    
    darcs-hash:541e3c0837cd0833d77c97075aa41dcda4a4ee6f

---
+++ benchees/dsp/doit11.c
@@ -0,0 +1,55 @@
+/* this program is in the public domain */
+
+#include "bench-user.h"
+#include <math.h>
+
+BEGIN_BENCH_DOC
+BENCH_DOC("name", "dsp79-rader")
+BENCH_DOC("author", "C. M. Rader, after Brenner")
+BENCH_DOC("year", "1979, after Brenner 1967")
+BENCH_DOC("language", "FORTRAN")
+BENCH_DOC("notes", "The backward transform is scaled")
+END_BENCH_DOC
+
+int can_do(struct problem *p)
+{
+     return (SINGLE_PRECISION &&
+	     p->rank == 1 &&
+	     problem_complex_power_of_two(p, 1) &&
+	     p->n[0] <= 32768);
+}
+
+void after_problem_ccopy_to(struct problem *p, bench_complex *out)
+{
+     if (p->sign == 1) { 	  /* undo the scaling */
+	  bench_complex x;
+	  c_re(x) = p->size;
+	  c_im(x) = 0;
+
+	  cascale(out, p->size, x);
+     }
+}
+
+extern void F77_FUNC(fourea, FOUREA)();
+
+void setup(struct problem *p)
+{
+     BENCH_ASSERT(can_do(p));
+}
+
+void doit(int iter, struct problem *p)
+{
+     int n = p->n[0];
+     int sign = p->sign;
+     bench_complex *in = p->in;
+     int i;
+
+     for (i = 0; i < iter; ++i) {
+	  F77_FUNC(fourea, FOUREA)(in, &n, &sign);
+     }
+}
+
+void done(struct problem *p)
+{
+     UNUSED(p);
+}
