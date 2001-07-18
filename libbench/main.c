aa635bceb38d462b3b1dc1e362521f5b3f53bb1b
stevenj <none>
2001-07-18 15:18:36 -0400

    [project @ 2001-07-18 19:18:36 by stevenj]
    take advantage of AC_F77_DUMMY_MAIN
    
    darcs-hash:94b65c503787781f09ecffc96023e1a758b1a2fa

--- libbench/main.c
+++ libbench/main.c
@@ -18,9 +18,20 @@
  *
  */
 
-/* $Id: main.c,v 1.1 2001-07-04 22:50:24 athena Exp $ */
+/* $Id: main.c,v 1.2 2001-07-18 19:18:36 stevenj Exp $ */
 
 #include "bench.h"
+
+/* On some systems, we are required to define a dummy main-like
+   routine (called "MAIN__" or something similar in order to link a C
+   main() with the Fortran libraries).  This is detected by autoconf;
+   see the autoconf 2.52 or later manual. */
+#ifdef F77_DUMMY_MAIN
+#  ifdef __cplusplus
+     extern "C"
+#  endif
+     int F77_DUMMY_MAIN() { return 1; }
+#endif
 
 /* in a separate file so that the user can override it */
 int main(int argc, char *argv[])
