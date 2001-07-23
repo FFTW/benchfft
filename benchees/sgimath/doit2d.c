a1db3217d0d21a2931b3dc78fb652e759ff05a17
athena <none>
2001-07-23 11:45:06 -0400

    [project @ 2001-07-23 15:45:06 by athena]
    Finished sgimath
    
    darcs-hash:1c4f345b31512895e9228da0c1d216d306c6bf74

--- benchees/sgimath/doit2d.c
+++ benchees/sgimath/doit2d.c
@@ -10,7 +10,7 @@ END_BENCH_DOC
 
 int can_do(struct problem *p)
 {
-     return (p->rank == 1 && problem_in_place(p));
+     return (p->rank == 2 && problem_in_place(p));
 }
 
 void copy_h2c(struct problem *p, bench_complex *out)
@@ -23,36 +23,49 @@ void copy_c2h(struct problem *p, bench_complex *in)
      copy_c2h_unpacked(p, in, -1.0);
 }
 
+
+void copy_r2c(struct problem *p, bench_complex *out)
+{
+     copy_r2c_unpacked(p, out);	  
+}
+
+void copy_c2r(struct problem *p, bench_complex *in)
+{
+     copy_c2r_unpacked(p, in);
+}
+
 #include <fft.h>
 
 static void *WSAVE;
 
 void setup(struct problem *p)
 {
-     int n;
+     int n1, n2;
  
      BENCH_ASSERT(can_do(p));
-     n = p->n[0];
+     n1 = p->n[1];
+     n2 = p->n[0];
  
      if (p->kind == PROBLEM_COMPLEX) {
-          WSAVE = bench_malloc((n + 15) * sizeof(bench_complex));
+          WSAVE = bench_malloc((n1 + n2 + 30) * sizeof(bench_complex));
 	  if (SINGLE_PRECISION) 
-	       cfft1di(n, WSAVE);
+	       cfft2di(n1, n2, WSAVE);
 	  else	       
-	       zfft1di(n, WSAVE);
+	       zfft2di(n1, n2, WSAVE);
      } else {
-          WSAVE = bench_malloc((n + 15) * sizeof(bench_real));
+          WSAVE = bench_malloc((n1 + 2 * n2 + 45) * sizeof(bench_real));
 	  if (SINGLE_PRECISION) 
-	       scfft1dui(n, WSAVE); /* works for both directions */
+	       scfft2dui(n1, n2, WSAVE); /* works for both directions */
 	  else	       
-	       dzfft1dui(n, WSAVE);
+	       dzfft2dui(n1, n2, WSAVE);
      }
 }
 
 void doit(int iter, struct problem *p)
 {
      int i;
-     int n = p->n[0];
+     int n1 = p->n[1];
+     int n2 = p->n[0];
      void *in = p->in;
      int sign = p->sign;
      void *wsave = WSAVE;
@@ -60,31 +73,32 @@ void doit(int iter, struct problem *p)
      if (p->kind == PROBLEM_COMPLEX) {
 	  if (SINGLE_PRECISION) {
 	       for (i = 0; i < iter; ++i) {
-		    cfft1d(sign, n, in, 1, wsave);
+		    cfft2d(sign, n1, n2, in, n1, wsave);
 	       }
 	  } else {
 	       for (i = 0; i < iter; ++i) {
-		    zfft1d(sign, n, in, 1, wsave);
+		    zfft2d(sign, n1, n2, in, n1, wsave);
 	       }
 	  }
      } else {
+          int lda = 2 * (1 + n1 / 2);
 	  if (p->sign == -1) {
 	       if (SINGLE_PRECISION) 
 		    for (i = 0; i < iter; ++i) {
-			 scfft1du(sign, n, in, 1,wsave);
+			 scfft2du(sign, n1, n2, in, lda, wsave);
 		    }
 	       else	
 		    for (i = 0; i < iter; ++i) {
-			 dzfft1du(sign, n, in, 1,wsave);
+			 dzfft2du(sign, n1, n2, in, lda, wsave);
 		    }
 	  } else {
 	       if (SINGLE_PRECISION) 
 		    for (i = 0; i < iter; ++i) {
-			 csfft1du(sign, n, in, 1,wsave);
+			 csfft2du(sign, n1, n2, in, lda, wsave);
 		    }
 	       else	
 		    for (i = 0; i < iter; ++i) {
-			 zdfft1du(sign, n, in, 1,wsave);
+			 zdfft2du(sign, n1, n2, in, lda, wsave);
 		    }
 	  }
      }
--- benchees/sgimath/doit3d.c
+++ benchees/sgimath/doit3d.c
@@ -10,7 +10,7 @@ END_BENCH_DOC
 
 int can_do(struct problem *p)
 {
-     return (p->rank == 1 && problem_in_place(p));
+     return (p->rank == 3 && problem_in_place(p));
 }
 
 void copy_h2c(struct problem *p, bench_complex *out)
@@ -23,36 +23,52 @@ void copy_c2h(struct problem *p, bench_complex *in)
      copy_c2h_unpacked(p, in, -1.0);
 }
 
+
+void copy_r2c(struct problem *p, bench_complex *out)
+{
+     copy_r2c_unpacked(p, out);	  
+}
+
+void copy_c2r(struct problem *p, bench_complex *in)
+{
+     copy_c2r_unpacked(p, in);
+}
+
 #include <fft.h>
 
 static void *WSAVE;
 
 void setup(struct problem *p)
 {
-     int n;
+     int n1, n2, n3;
  
      BENCH_ASSERT(can_do(p));
-     n = p->n[0];
+     n1 = p->n[2];
+     n2 = p->n[1];
+     n3 = p->n[0];
  
      if (p->kind == PROBLEM_COMPLEX) {
-          WSAVE = bench_malloc((n + 15) * sizeof(bench_complex));
+          WSAVE = bench_malloc((n1 + n2 + n3 + 45) * sizeof(bench_complex));
 	  if (SINGLE_PRECISION) 
-	       cfft1di(n, WSAVE);
+	       cfft3di(n1, n2, n3, WSAVE);
 	  else	       
-	       zfft1di(n, WSAVE);
+	       zfft3di(n1, n2, n3, WSAVE);
      } else {
-          WSAVE = bench_malloc((n + 15) * sizeof(bench_real));
+          WSAVE = bench_malloc((n1 + 2 * n2 + 2 * n3 + 75)
+			       * sizeof(bench_real));
 	  if (SINGLE_PRECISION) 
-	       scfft1dui(n, WSAVE); /* works for both directions */
+	       scfft3dui(n1, n2, n3, WSAVE); /* works for both directions */
 	  else	       
-	       dzfft1dui(n, WSAVE);
+	       dzfft3dui(n1, n2, n3, WSAVE);
      }
 }
 
 void doit(int iter, struct problem *p)
 {
      int i;
-     int n = p->n[0];
+     int n1 = p->n[2];
+     int n2 = p->n[1];
+     int n3 = p->n[0];
      void *in = p->in;
      int sign = p->sign;
      void *wsave = WSAVE;
@@ -60,31 +76,32 @@ void doit(int iter, struct problem *p)
      if (p->kind == PROBLEM_COMPLEX) {
 	  if (SINGLE_PRECISION) {
 	       for (i = 0; i < iter; ++i) {
-		    cfft1d(sign, n, in, 1, wsave);
+		    cfft3d(sign, n1, n2, n3, in, n1, n2, wsave);
 	       }
 	  } else {
 	       for (i = 0; i < iter; ++i) {
-		    zfft1d(sign, n, in, 1, wsave);
+		    zfft3d(sign, n1, n2, n3, in, n1, n2, wsave);
 	       }
 	  }
      } else {
+          int lda = 2 * (1 + n1 / 2);
 	  if (p->sign == -1) {
 	       if (SINGLE_PRECISION) 
 		    for (i = 0; i < iter; ++i) {
-			 scfft1du(sign, n, in, 1,wsave);
+			 scfft3du(sign, n1, n2, n3, in, lda, n2, wsave);
 		    }
 	       else	
 		    for (i = 0; i < iter; ++i) {
-			 dzfft1du(sign, n, in, 1,wsave);
+			 dzfft3du(sign, n1, n2, n3, in, lda, n2, wsave);
 		    }
 	  } else {
 	       if (SINGLE_PRECISION) 
 		    for (i = 0; i < iter; ++i) {
-			 csfft1du(sign, n, in, 1,wsave);
+			 csfft3du(sign, n1, n2, n3, in, lda, n2, wsave);
 		    }
 	       else	
 		    for (i = 0; i < iter; ++i) {
-			 zdfft1du(sign, n, in, 1,wsave);
+			 zdfft3du(sign, n1, n2, n3, in, lda, n2, wsave);
 		    }
 	  }
      }
