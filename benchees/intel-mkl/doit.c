07389f7cb520b4b4dce478ab58ef8e83bd6d6fc5
athena <none>
2001-07-23 10:08:33 -0400

    [project @ 2001-07-23 14:08:33 by athena]
    Finished MKL routines
    
    darcs-hash:db41652ed645311fffa48c5ebb82b797eb3a73cb

--- benchees/intel-mkl/doit.c
+++ benchees/intel-mkl/doit.c
@@ -8,32 +8,163 @@ BENCH_DOC("name", NAME)
 BENCH_DOC("notes", "backward transform is scaled")
 END_BENCH_DOC
 
-
 int can_do(struct problem *p)
 {
-     return (p->rank == 1 && problem_power_of_two(p, 1));
+     return (p->rank >= 1 && p->rank <= 2 && problem_power_of_two(p, 1));
+}
+
+/*
+
+The following routine comes from Intel's library examples.  I am glad
+I don't have to use this screwed format.  The routine is (C) Intel and
+it is distributed accorging to the following excerpt from the license:
+
+<p class=MsoPlainText><b>Sample Source:</b> may include example interface or
+application source code.<span style="mso-spacerun: yes">  </span>You may copy,
+modify and compile the Sample Source and distribute it in your own products in
+binary and source code form.<o:p></o:p></p> 
+*/
+
+static void dz_copy(bench_real *a, bench_complex *o, int m, int n)
+{
+     int i, j;
+     int n2, m2, nn;
+
+     n2 = n + 2;
+     m2 = m + 2;
+
+     c_re(o[0]) = a[0];
+     c_im(o[0]) = 0.0;
+
+     if (m > 1) {
+	  c_re(o[(m / 2) * n]) = a[(m / 2) * n2];
+	  c_im(o[(m / 2) * n]) = 0.0;
+     }
+
+     for (i = 1; i < m / 2; i++) {
+	  c_re(o[i * n]) = a[i * n2];
+	  c_im(o[i * n]) = a[(m / 2 + i + 1) * n2];
+	  c_re(o[(m - i) * n]) = a[i * n2];
+	  c_im(o[(m - i) * n]) = -a[(m / 2 + i + 1) * n2];
+     }
+
+     if (n > 1) {
+	  nn = n / 2;
+	  c_re(o[nn]) = a[nn];
+	  c_im(o[nn]) = 0.0;
+
+	  if (m > 1) {
+	       c_re(o[(m / 2) * n + nn]) = a[(m / 2) * n2 + nn];
+	       c_im(o[(m / 2) * n + nn]) = 0.0;
+	  }
+
+	  for (i = 1; i < m / 2; i++) {
+	       c_re(o[i * n + nn]) = a[i * n2 + nn];
+	       c_im(o[i * n + nn]) = a[(m / 2 + i + 1) * n2 + nn];
+	       c_re(o[(m - i) * n + nn]) = a[i * n2 + nn];
+	       c_im(o[(m - i) * n + nn]) = -a[(m / 2 + i + 1) * n2 + nn];
+	  }
+     }
+
+
+     for (j = 1; j < n / 2; j++) {
+	  c_re(o[j]) = a[j];
+	  c_im(o[j]) = a[n / 2 + j + 1];
+	  c_re(o[n - j]) = a[j];
+	  c_im(o[n - j]) = -a[n / 2 + j + 1];
+	  for (i = 1; i < m; i++) {
+	       c_re(o[i * n + j]) = a[i * n2 + j];
+	       c_im(o[i * n + j]) = a[i * n2 + n / 2 + j + 1];
+	       c_re(o[(m - i) * n + n - j]) = a[i * n2 + j];
+	       c_im(o[(m - i) * n + n - j]) = -a[i * n2 + n / 2 + j + 1];
+	  }
+     }
+
+}
+
+/* complex->hermitian 2d copy routine */
+static void zd_copy(bench_real *a, bench_complex *o, int m, int n)
+{
+     int i, j;
+     int n2, m2, nn;
+
+     n2 = n + 2;
+     m2 = m + 2;
+
+     a[0] = c_re(o[0]);
+
+     if (m > 1) {
+	  a[(m / 2) * n2] = c_re(o[(m / 2) * n]);
+     }
+
+     for (i = 1; i < m / 2; i++) {
+	  a[i * n2] = c_re(o[i * n]);
+	  a[(m / 2 + i + 1) * n2] = c_im(o[i * n]);
+     }
+
+     if (n > 1) {
+	  nn = n / 2;
+	  a[nn] = c_re(o[nn]);
+
+	  if (m > 1) {
+	       a[(m / 2) * n2 + nn] = c_re(o[(m / 2) * n + nn]);
+	  }
+
+	  for (i = 1; i < m / 2; i++) {
+	       a[i * n2 + nn] = c_re(o[i * n + nn]);
+	       a[(m / 2 + i + 1) * n2 + nn] = c_im(o[i * n + nn]);
+	  }
+     }
+
+
+     for (j = 1; j < n / 2; j++) {
+	  a[j] = c_re(o[j]);
+	  a[n / 2 + j + 1] = c_im(o[j]);
+	  for (i = 1; i < m; i++) {
+	       a[i * n2 + j] = c_re(o[i * n + j]);
+	       a[i * n2 + n / 2 + j + 1] = c_im(o[i * n + j]);
+	  }
+     }
 }
 
 void copy_h2c(struct problem *p, bench_complex *out)
 {
-     copy_h2c_1d_unpacked_ri(p, out, -1.0);
+     if (p->rank == 1) {
+	  copy_h2c_1d_unpacked_ri(p, out, -1.0);
+     } else {
+	  dz_copy(p->out, out, p->n[0], p->n[1]);
+     }
 }
 
 
 void copy_c2h(struct problem *p, bench_complex *in)
 {
-     copy_c2h_1d_unpacked_ri(p, in, -1.0);
+     if (p->rank == 1) {
+	  copy_c2h_1d_unpacked_ri(p, in, -1.0);
+     } else {
+	  zd_copy(p->in, in, p->n[0], p->n[1]);
+     }
 }
 
 
+void copy_r2c(struct problem *p, bench_complex *out)
+{
+     copy_r2c_unpacked(p, out);
+}
+
+void copy_c2r(struct problem *p, bench_complex *in)
+{
+     copy_c2r_unpacked(p, in);
+}
+
 void copy_c2c_from(struct problem *p, bench_complex *in)
 {
-     copy_c2ri(in, p->in, ((bench_real *)p->in) + p->n[0], p->n[0]);
+     copy_c2ri(in, p->in, ((bench_real *) p->in) + p->size, p->size);
 }
 
 void copy_c2c_to(struct problem *p, bench_complex *out)
 {
-     copy_ri2c(p->out, ((bench_real *)p->out) + p->n[0], out, p->n[0]);
+     copy_ri2c(p->out, ((bench_real *) p->out) + p->size, out, p->size);
 }
 
 void after_problem_ccopy_to(struct problem *p, bench_complex *out)
@@ -54,82 +185,141 @@ static void *WSAVE;
 void setup(struct problem *p)
 {
      int n;
- 
+
      BENCH_ASSERT(can_do(p));
-     n = p->n[0];
- 
-     if (p->kind == PROBLEM_COMPLEX) {
-	  bench_real *rin = p->in;
-	  bench_real *iin = rin + n;
 
-	  /*
-	   * example code says that wsave consists of 3 * n
-	   * locations, but the code dumps core for n == 4
-	   */
-          WSAVE = bench_malloc((3 * n + 4) * sizeof(bench_real));
-	  if (SINGLE_PRECISION) 
-	       CFFT1DC(rin, iin, n, 0, WSAVE);
-	  else	       
-	       ZFFT1DC(rin, iin, n, 0, WSAVE);
-     } else {
-          WSAVE = bench_malloc((4 * n) * sizeof(bench_real));
+     switch (p->rank) {
+     case 1:
+	  {
 
-	  if (p->sign == -1) {
-	       if (SINGLE_PRECISION) 
-		    SCFFT1DC(p->in, n, 0, WSAVE);
-	       else	       
-		    DZFFT1DC(p->in, n, 0, WSAVE);
-	  } else {
-	       if (SINGLE_PRECISION) 
-		    CSFFT1DC(p->in, n, 0, WSAVE);
-	       else	       
-		    ZDFFT1DC(p->in, n, 0, WSAVE);
+	       n = p->n[0];
+
+	       if (p->kind == PROBLEM_COMPLEX) {
+		    bench_real *rin = p->in;
+		    bench_real *iin = rin + n;
+
+		    /*
+		     * example code says that wsave consists of 3 * n
+		     * locations, but the code dumps core for n == 4
+		     */
+		    WSAVE = bench_malloc((3 * n + 4) * sizeof(bench_real));
+		    if (SINGLE_PRECISION)
+			 CFFT1DC(rin, iin, n, 0, WSAVE);
+		    else
+			 ZFFT1DC(rin, iin, n, 0, WSAVE);
+	       } else {
+		    WSAVE = bench_malloc((4 * n) * sizeof(bench_real));
+
+		    if (p->sign == -1) {
+			 if (SINGLE_PRECISION)
+			      SCFFT1DC(p->in, n, 0, WSAVE);
+			 else
+			      DZFFT1DC(p->in, n, 0, WSAVE);
+		    } else {
+			 if (SINGLE_PRECISION)
+			      CSFFT1DC(p->in, n, 0, WSAVE);
+			 else
+			      ZDFFT1DC(p->in, n, 0, WSAVE);
+		    }
+	       }
+	       break;
 	  }
+
+     case 2:
+	  /* nothing to do */
+	  break;
+     default:
+	  BENCH_ASSERT(0);
      }
 }
 
 void doit(int iter, struct problem *p)
 {
      int i;
-     int n = p->n[0];
-     void *wsave = WSAVE;
+     int sign = p->sign;
 
-     if (p->kind == PROBLEM_COMPLEX) {
-	  bench_real *rin = p->in;
-	  bench_real *iin = rin + n;
-	  int sign = p->sign;
+     switch (p->rank) {
+     case 1:{
+	       void *wsave = WSAVE;
+	       int n = p->n[0];
+	       if (p->kind == PROBLEM_COMPLEX) {
+		    bench_real *rin = p->in;
+		    bench_real *iin = rin + n;
 
-	  if (SINGLE_PRECISION) {
-	       for (i = 0; i < iter; ++i) {
-		    CFFT1DC(rin, iin, n, sign, wsave);
+		    if (SINGLE_PRECISION) {
+			 for (i = 0; i < iter; ++i) {
+			      CFFT1DC(rin, iin, n, sign, wsave);
+			 }
+		    } else {
+			 for (i = 0; i < iter; ++i) {
+			      ZFFT1DC(rin, iin, n, sign, wsave);
+			 }
+		    }
+	       } else {
+		    bench_real *pin = p->in;
+		    if (p->sign == -1) {
+			 if (SINGLE_PRECISION)
+			      for (i = 0; i < iter; ++i) {
+				   SCFFT1DC(pin, n, -1, WSAVE);
+			      }
+			 else
+			      for (i = 0; i < iter; ++i) {
+				   DZFFT1DC(pin, n, -1, WSAVE);
+			      }
+		    } else {
+			 if (SINGLE_PRECISION)
+			      for (i = 0; i < iter; ++i) {
+				   CSFFT1DC(pin, n, 1, WSAVE);
+			      } 
+			 else
+			      for (i = 0; i < iter; ++i) {
+				   ZDFFT1DC(pin, n, 1, WSAVE);
+			      }
+		    }
 	       }
-	  } else {
-	       for (i = 0; i < iter; ++i) {
-		    ZFFT1DC(rin, iin, n, sign, wsave);
+	       break;
+	  }
+     case 2:
+	  {
+	       int m = p->n[0];
+	       int n = p->n[1];
+
+	       if (p->kind == PROBLEM_COMPLEX) {
+		    bench_real *rpin = p->in;
+		    bench_real *ipin = rpin + p->size;
+
+		    if (SINGLE_PRECISION) {
+			 for (i = 0; i < iter; ++i) {
+			      CFFT2DC(rpin, ipin, m, n, sign);
+			 }
+		    } else {
+			 for (i = 0; i < iter; ++i) {
+			      ZFFT2DC(rpin, ipin, m, n, sign);
+			 }
+		    }
+	       } else {
+		    bench_real *pin = p->in;
+		    if (sign == -1) {
+			 if (SINGLE_PRECISION)
+			      for (i = 0; i < iter; ++i) {
+				   SCFFT2DC(pin, m, n);
+			      } 
+			 else
+			      for (i = 0; i < iter; ++i) {
+				   DZFFT2DC(pin, m, n);
+			      }
+		    } else {
+			 if (SINGLE_PRECISION)
+			      for (i = 0; i < iter; ++i) {
+				   CSFFT2DC(pin, m, n);
+			      } 
+			 else
+			      for (i = 0; i < iter; ++i) {
+				   ZDFFT2DC(pin, m, n);
+			      }
+		    }
 	       }
 	  }
-     } else {
-	  bench_real *pin = p->in;
-	  if (p->sign == -1) {
-	       if (SINGLE_PRECISION) 
-		    for (i = 0; i < iter; ++i) {
-			 SCFFT1DC(pin, n, -1, WSAVE);
-		    }
-	       else	       
-		    for (i = 0; i < iter; ++i) {
-			 DZFFT1DC(pin, n, -1, WSAVE);
-		    }
-	  } else {
-	       if (SINGLE_PRECISION) 
-		    for (i = 0; i < iter; ++i) {
-			 CSFFT1DC(pin, n, 1, WSAVE);
-		    }
-	       else	       
-		    for (i = 0; i < iter; ++i) {
-			 ZDFFT1DC(pin, n, 1, WSAVE);
-		    }
-	  }
-
      }
 }
 
--- benchees/intel-mkl/doitf.c
+++ benchees/intel-mkl/doitf.c
@@ -3,28 +3,227 @@
 #include "bench-user.h"
 #include <math.h>
 
-BEGIN_BENCH_DOC
+BEGIN_BENCH_DOC 
 BENCH_DOC("name", NAME)
 BENCH_DOC("notes", "backward transform is scaled")
-END_BENCH_DOC
-
+END_BENCH_DOC 
 
 int can_do(struct problem *p)
 {
-     return (p->rank == 1 && problem_power_of_two(p, 1));
+     return (p->rank >= 1 && p->rank <= 2 && problem_power_of_two(p, 1));
 }
+
+
+/*
+
+The following routine comes from Intel's library examples.  I am glad
+I don't have to use this screwed format.  The routine was translated
+by f2c and massaged by hand.  The routine is (C) Intel and it is
+distributed accorging to the following excerpt from the license:
+
+
+<p class=MsoPlainText><b>Sample Source:</b> may include example interface or
+application source code.<span style="mso-spacerun: yes">  </span>You may copy,
+modify and compile the Sample Source and distribute it in your own products in
+binary and source code form.<o:p></o:p></p>
+
+ */
+
+static void dzmcopy(bench_real *ra, int rlda, bench_complex *ca,
+		    int clda, int m, int n)
+{
+     /* System generated locals */
+     int ra_dim1, ra_offset, ca_dim1, ca_offset, i1, i2, i3, i4, i5;
+     bench_real d1;
+
+     /* Local variables */
+     int i, j, k, mm;
+
+/* --- Input Arguments */
+/* --- Output Arguments */
+/* --- Local variables */
+     /* Parameter adjustments */
+     ra_dim1 = rlda;
+     ra_offset = 1 + ra_dim1 * 1;
+     ra -= ra_offset;
+     ca_dim1 = clda;
+     ca_offset = 1 + ca_dim1 * 1;
+     ca -= ca_offset;
+
+     /* Function Body */
+     i1 = ca_dim1 + 1;
+     i2 = ra_dim1 + 1;
+     ca[i1].re = ra[i2], ca[i1].im = 0.;
+     if (n > 1) {
+	  i1 = (n / 2 + 1) * ca_dim1 + 1;
+	  i2 = (n + 1) * ra_dim1 + 1;
+	  ca[i1].re = ra[i2], ca[i1].im = 0.;
+     }
+     k = 3;
+     i1 = n / 2;
+     for (j = 2; j <= i1; ++j) {
+	  i2 = j * ca_dim1 + 1;
+	  i3 = k * ra_dim1 + 1;
+	  i4 = (k + 1) * ra_dim1 + 1;
+	  ca[i2].re = ra[i3], ca[i2].im = ra[i4];
+	  i2 = (n + 2 - j) * ca_dim1 + 1;
+	  i3 = k * ra_dim1 + 1;
+	  d1 = -ra[(k + 1) * ra_dim1 + 1];
+	  ca[i2].re = ra[i3], ca[i2].im = d1;
+	  k += 2;
+     }
+     if (m > 1) {
+	  mm = m / 2 + 1;
+	  i1 = mm + ca_dim1;
+	  i2 = m + 1 + ra_dim1;
+	  ca[i1].re = ra[i2], ca[i1].im = 0;
+	  if (n > 1) {
+	       i1 = mm + (n / 2 + 1) * ca_dim1;
+	       i2 = m + 1 + (n + 1) * ra_dim1;
+	       ca[i1].re = ra[i2], ca[i1].im = 0;
+	  }
+	  k = 3;
+	  i1 = n / 2;
+	  for (j = 2; j <= i1; ++j) {
+	       i2 = mm + j * ca_dim1;
+	       i3 = m + 1 + k * ra_dim1;
+	       i4 = m + 1 + (k + 1) * ra_dim1;
+	       ca[i2].re = ra[i3], ca[i2].im = ra[i4];
+	       i2 = mm + (n + 2 - j) * ca_dim1;
+	       i3 = m + 1 + k * ra_dim1;
+	       d1 = -ra[m + 1 + (k + 1) * ra_dim1];
+	       ca[i2].re = ra[i3], ca[i2].im = d1;
+	       k += 2;
+	  }
+     }
+     k = 3;
+     i1 = m / 2;
+     for (i = 2; i <= i1; ++i) {
+	  i2 = i + ca_dim1;
+	  i3 = k + ra_dim1;
+	  i4 = k + 1 + ra_dim1;
+	  ca[i2].re = ra[i3], ca[i2].im = ra[i4];
+	  i2 = m + 2 - i + ca_dim1;
+	  i3 = k + ra_dim1;
+	  d1 = -ra[k + 1 + ra_dim1];
+	  ca[i2].re = ra[i3], ca[i2].im = d1;
+	  i2 = n;
+	  for (j = 2; j <= i2; ++j) {
+	       i3 = i + j * ca_dim1;
+	       i4 = k + j * ra_dim1;
+	       i5 = k + 1 + j * ra_dim1;
+	       ca[i3].re = ra[i4], ca[i3].im = ra[i5];
+	       i3 = m + 2 - i + (n + 2 - j) * ca_dim1;
+	       i4 = k + j * ra_dim1;
+	       d1 = -ra[k + 1 + j * ra_dim1];
+	       ca[i3].re = ra[i4], ca[i3].im = d1;
+	  }
+	  k += 2;
+     }
+}				/* dzmcopy_ */
+
+
+static void zdmcopy(bench_real *ra, int rlda, bench_complex *ca,
+		    int clda, int m, int n)
+{
+     /* System generated locals */
+     int ra_dim1, ra_offset, ca_dim1, ca_offset, i1, i2, i3, i4, i5;
+
+     /* Local variables */
+     int i, j, k, mm;
+
+     /* Parameter adjustments */
+     ra_dim1 = rlda;
+     ra_offset = 1 + ra_dim1 * 1;
+     ra -= ra_offset;
+     ca_dim1 = clda;
+     ca_offset = 1 + ca_dim1 * 1;
+     ca -= ca_offset;
+
+     /* Function Body */
+     i1 = ca_dim1 + 1;
+     i2 = ra_dim1 + 1;
+     ra[i2] = ca[i1].re;
+     if (n > 1) {
+	  i1 = (n / 2 + 1) * ca_dim1 + 1;
+	  i2 = (n + 1) * ra_dim1 + 1;
+	  ra[i2] = ca[i1].re;
+     }
+     k = 3;
+     i1 = n / 2;
+     for (j = 2; j <= i1; ++j) {
+	  i2 = j * ca_dim1 + 1;
+	  i3 = k * ra_dim1 + 1;
+	  i4 = (k + 1) * ra_dim1 + 1;
+	  ra[i3] = ca[i2].re, ra[i4] = ca[i2].im;
+	  k += 2;
+     }
+     if (m > 1) {
+	  mm = m / 2 + 1;
+	  i1 = mm + ca_dim1;
+	  i2 = m + 1 + ra_dim1;
+	  ra[i2] = ca[i1].re;
+	  if (n > 1) {
+	       i1 = mm + (n / 2 + 1) * ca_dim1;
+	       i2 = m + 1 + (n + 1) * ra_dim1;
+	       ra[i2] = ca[i1].re;
+	  }
+	  k = 3;
+	  i1 = n / 2;
+	  for (j = 2; j <= i1; ++j) {
+	       i2 = mm + j * ca_dim1;
+	       i3 = m + 1 + k * ra_dim1;
+	       i4 = m + 1 + (k + 1) * ra_dim1;
+	       ra[i3] = ca[i2].re, ra[i4] = ca[i2].im;
+	       k += 2;
+	  }
+     }
+     k = 3;
+     i1 = m / 2;
+     for (i = 2; i <= i1; ++i) {
+	  i2 = i + ca_dim1;
+	  i3 = k + ra_dim1;
+	  i4 = k + 1 + ra_dim1;
+	  ra[i3] = ca[i2].re, ra[i4] = ca[i2].im;
+	  i2 = n;
+	  for (j = 2; j <= i2; ++j) {
+	       i3 = i + j * ca_dim1;
+	       i4 = k + j * ra_dim1;
+	       i5 = k + 1 + j * ra_dim1;
+	       ra[i4] = ca[i3].re, ra[i5] = ca[i3].im;
+	  }
+	  k += 2;
+     }
+}
+
 
 void copy_h2c(struct problem *p, bench_complex *out)
 {
-     copy_h2c_unpacked(p, out, -1.0);
+     if (p->rank == 1) {
+	  copy_h2c_unpacked(p, out, -1.0);
+     } else {
+	  dzmcopy(p->out, p->n[1] + 2, out, p->n[1], p->n[1], p->n[0]);
+     }
 }
-
 
 void copy_c2h(struct problem *p, bench_complex *in)
 {
-     copy_c2h_unpacked(p, in, -1.0);
+     if (p->rank == 1) {
+	  copy_c2h_unpacked(p, in, -1.0);
+     } else {
+	  zdmcopy(p->in, p->n[1] + 2, in, p->n[1], p->n[1], p->n[0]);
+     }
 }
 
+void copy_r2c(struct problem *p, bench_complex *out)
+{
+     copy_r2c_unpacked(p, out);
+}
+
+void copy_c2r(struct problem *p, bench_complex *in)
+{
+     copy_c2r_unpacked(p, in);
+}
 
 void after_problem_ccopy_to(struct problem *p, bench_complex *out)
 {
@@ -45,83 +244,145 @@ void after_problem_ccopy_to(struct problem *p, bench_complex *out)
 #define SCFFT1D scfft1d_
 #define CSFFT1D csfft1d_
 
+#define ZFFT2D zfft2d_
+#define CFFT2D cfft2d_
+#define DZFFT2D dzfft2d_
+#define ZDFFT2D zdfft2d_
+#define SCFFT2D scfft2d_
+#define CSFFT2D csfft2d_
+
 extern void ZFFT1D(), CFFT1D(), DZFFT1D(), ZDFFT1D(), SCFFT1D(), CSFFT1D();
+extern void ZFFT2D(), CFFT2D(), DZFFT2D(), ZDFFT2D(), SCFFT2D(), CSFFT2D();
 static void *WSAVE;
 
 void setup(struct problem *p)
 {
      int n, zero = 0;
- 
-     BENCH_ASSERT(can_do(p));
-     n = p->n[0];
- 
-     if (p->kind == PROBLEM_COMPLEX) {
-	  /*
-	   * example code says that wsave consists of 3 * n
-	   * locations, but the code dumps core for n == 4
-	   */
-          WSAVE = bench_malloc((3 * n + 4) * sizeof(bench_real));
-	  if (SINGLE_PRECISION) 
-	       CFFT1D(p->in, &n, &zero, WSAVE);
-	  else	       
-	       ZFFT1D(p->in, &n, &zero, WSAVE);
-     } else {
-          WSAVE = bench_malloc((4 * n) * sizeof(bench_real));
 
-	  if (p->sign == -1) {
-	       if (SINGLE_PRECISION) 
-		    SCFFT1D(p->in, &n, &zero, WSAVE);
-	       else	       
-		    DZFFT1D(p->in, &n, &zero, WSAVE);
-	  } else {
-	       if (SINGLE_PRECISION) 
-		    CSFFT1D(p->in, &n, &zero, WSAVE);
-	       else	       
-		    ZDFFT1D(p->in, &n, &zero, WSAVE);
+     BENCH_ASSERT(can_do(p));
+
+     switch (p->rank) {
+     case 1:
+	  {
+	       n = p->n[0];
+
+	       if (p->kind == PROBLEM_COMPLEX) {
+		    /*
+		     * example code says that wsave consists of 3 * n
+		     * locations, but the code dumps core for n == 4
+		     */
+		    WSAVE = bench_malloc((3 * n + 4) * sizeof(bench_real));
+		    if (SINGLE_PRECISION)
+			 CFFT1D(p->in, &n, &zero, WSAVE);
+		    else
+			 ZFFT1D(p->in, &n, &zero, WSAVE);
+	       } else {
+		    WSAVE = bench_malloc((4 * n) * sizeof(bench_real));
+
+		    if (p->sign == -1) {
+			 if (SINGLE_PRECISION)
+			      SCFFT1D(p->in, &n, &zero, WSAVE);
+			 else
+			      DZFFT1D(p->in, &n, &zero, WSAVE);
+		    } else {
+			 if (SINGLE_PRECISION)
+			      CSFFT1D(p->in, &n, &zero, WSAVE);
+			 else
+			      ZDFFT1D(p->in, &n, &zero, WSAVE);
+		    }
+	       }
+	       break;
 	  }
+     case 2:
+	  /* nothing to do */
+	  break;
+     default:
+	  BENCH_ASSERT(0);
      }
 }
 
 void doit(int iter, struct problem *p)
 {
      int i;
-     int n = p->n[0];
-     void *wsave = WSAVE;
      int sign = p->sign;
 
-     if (p->kind == PROBLEM_COMPLEX) {
-	  bench_real *pin = p->in;
+     switch (p->rank) {
+     case 1:{
+	       int n = p->n[0];
+	       void *wsave = WSAVE;
 
-	  if (SINGLE_PRECISION) {
-	       for (i = 0; i < iter; ++i) {
-		    CFFT1D(pin, &n, &sign, wsave);
+	       if (p->kind == PROBLEM_COMPLEX) {
+		    bench_real *pin = p->in;
+
+		    if (SINGLE_PRECISION) {
+			 for (i = 0; i < iter; ++i) {
+			      CFFT1D(pin, &n, &sign, wsave);
+			 }
+		    } else {
+			 for (i = 0; i < iter; ++i) {
+			      ZFFT1D(pin, &n, &sign, wsave);
+			 }
+		    }
+	       } else {
+		    bench_real *pin = p->in;
+
+		    if (sign == -1) {
+			 if (SINGLE_PRECISION)
+			      for (i = 0; i < iter; ++i) {
+				   SCFFT1D(pin, &n, &sign, WSAVE);
+			      } 
+			 else
+			      for (i = 0; i < iter; ++i) {
+				   DZFFT1D(pin, &n, &sign, WSAVE);
+			      }
+		    } else {
+			 if (SINGLE_PRECISION)
+			      for (i = 0; i < iter; ++i) {
+				   CSFFT1D(pin, &n, &sign, WSAVE);
+			      } 
+			 else
+			      for (i = 0; i < iter; ++i) {
+				   ZDFFT1D(pin, &n, &sign, WSAVE);
+			      }
+		    }
 	       }
-	  } else {
-	       for (i = 0; i < iter; ++i) {
-		    ZFFT1D(pin, &n, &sign, wsave);
-	       }
+	       break;
 	  }
-     } else {
-	  bench_real *pin = p->in;
+     case 2:{
+	       int n = p->n[0];
+	       int m = p->n[1];
+	       bench_real *pin = p->in;
 
-	  if (sign == -1) {
-	       if (SINGLE_PRECISION) 
-		    for (i = 0; i < iter; ++i) {
-			 SCFFT1D(pin, &n, &sign, WSAVE);
+	       if (p->kind == PROBLEM_COMPLEX) {
+		    if (SINGLE_PRECISION) {
+			 for (i = 0; i < iter; ++i) {
+			      CFFT2D(pin, &m, &n, &sign);
+			 }
+		    } else {
+			 for (i = 0; i < iter; ++i) {
+			      ZFFT2D(pin, &m, &n, &sign);
+			 }
 		    }
-	       else	       
-		    for (i = 0; i < iter; ++i) {
-			 DZFFT1D(pin, &n, &sign, WSAVE);
+	       } else {
+		    if (sign == -1) {
+			 if (SINGLE_PRECISION)
+			      for (i = 0; i < iter; ++i) {
+				   SCFFT2D(pin, &m, &n);
+			      }
+			 else
+			      for (i = 0; i < iter; ++i) {
+				   DZFFT2D(pin, &m, &n);
+			      }
+		    } else {
+			 if (SINGLE_PRECISION)
+			      for (i = 0; i < iter; ++i) {
+				   CSFFT2D(pin, &m, &n);
+			 } else
+			      for (i = 0; i < iter; ++i) {
+				   ZDFFT2D(pin, &m, &n);
+			      }
 		    }
-	  } else {
-	       if (SINGLE_PRECISION) 
-		    for (i = 0; i < iter; ++i) {
-			 CSFFT1D(pin, &n, &sign, WSAVE);
-		    }
-	       else	       
-		    for (i = 0; i < iter; ++i) {
-			 ZDFFT1D(pin, &n, &sign, WSAVE);
-		    }
+	       }
 	  }
 
      }
@@ -130,5 +391,6 @@ void doit(int iter, struct problem *p)
 void done(struct problem *p)
 {
      UNUSED(p);
-     bench_free(WSAVE);
+     if (p->rank == 1)
+	  bench_free(WSAVE);
 }
