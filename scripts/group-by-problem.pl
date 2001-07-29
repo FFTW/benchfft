2bfdbd054a5fbf7be5fd7142cae1292177c52263
athena <none>
2001-07-29 14:47:55 -0400

    [project @ 2001-07-29 18:47:55 by athena]
    Account for failures
    
    darcs-hash:7e1dc3f9fd4ff8aa4d001419a5402975d26ce10d

--- scripts/group-by-problem.pl
+++ scripts/group-by-problem.pl
@@ -7,6 +7,10 @@ while (<>) {
   my ($nam, $prob, $siz, $mflops, $tim) = split / /;
   my $prob0 = $prob . $siz;
   if ($prob0 ne $oprob) { print "\n"; print "$prob $siz:\n"; }
-  printf("%30s %12.2f %14.5g\n", $nam, $mflops, $tim);
+  if ($mflops eq "FAILED") {
+    printf("%30s %12s\n", $nam, $mflops);
+  } else {
+    printf("%30s %12.2f %14.5g\n", $nam, $mflops, $tim);
+  }
   $oprob=$prob0;
 }
