b25adbaf093f0227b19d4fc95f635b772c3ff48f
athena <none>
2001-07-29 07:46:02 -0400

    [project @ 2001-07-29 11:46:02 by athena]
    Account for FAILED results
    
    darcs-hash:129c548d507df8021ef0d2e557c4232b084a5327

--- scripts/sort-by-problem.pl
+++ scripts/sort-by-problem.pl
@@ -1,14 +1,24 @@
 #! /usr/bin/perl -w
 
+my @records=();
+my @probs=();
+my @ranks=();
+my @tots=();
+my @sizs=();
+my @mflops=();
+my @nams=();
 while (<>) {
   push(@records, $_);
   my ($nam, $prob, $siz, $mflops, $tim) = split / /;
   my $rank = ($siz =~ s/x/*/g);
   my $tot = eval($siz);
+  push(@nams, $nam);
   push(@probs, $prob);
   push(@ranks, $rank);
   push(@tots, $tot);
   push(@sizs, $siz);
+
+  $mflops=0 if ($mflops eq "FAILED");
   push(@mflopss, $mflops);
 }
 
@@ -18,6 +28,7 @@ while (<>) {
   $tots[$a] <=> $tots[$b] ||      # if tie, sort by total transform size
   $sizs[$a] cmp $sizs[$b] ||      # if tie, sort alphabetically on problem description
   $mflopss[$b] <=> $mflopss[$a];  # if tie, reverse sort by speed
+  $nams[$a] cmp $nams[$b]         # if tie, sort  by name
   } 0..$#records
 ];
 
