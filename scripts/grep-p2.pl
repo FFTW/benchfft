c5ae8a6e54a27d44a736b43cd5a96a8e2d8e8bae
athena <none>
2001-08-11 07:44:38 -0400

    [project @ 2001-08-11 11:44:37 by athena]
    New scripts
    
    darcs-hash:cf5094ddf2c06139c612f2f03b136d23a2a5dc28

---
+++ scripts/grep-p2.pl
@@ -0,0 +1,14 @@
+#! /usr/bin/perl -w
+
+# print only power-of-two problems
+
+while (<>) {
+  my ($nam, $prob, $siz, $mflops, $tim) = split / /;
+  my $rank = ($siz =~ s/x/*/g);
+  my $tot = eval($siz);
+
+  if (($tot & ($tot - 1)) == 0) {
+    print;
+  }
+}
+
---
+++ scripts/grep-rank.pl
@@ -0,0 +1,17 @@
+#! /usr/bin/perl -w
+
+# print only problems of rank argv[1]
+
+@ARGV > 0 || die "usage: $0 RANK";
+$r=shift;
+
+while (<>) {
+  my ($nam, $prob, $siz, $mflops, $tim) = split / /;
+  my $rank = 1 + ($siz =~ s/x/*/g);
+  my $tot = eval($siz);
+
+  if ($rank == $r) {
+    print;
+  }
+}
+
