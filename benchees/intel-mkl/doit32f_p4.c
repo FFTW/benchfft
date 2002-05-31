729a55979ec039a71925401f3ee6bb04fc93d490
athena <none>
2002-05-31 08:52:46 -0400

    [project @ 2002-05-31 12:52:46 by athena]
    Fixed
    
    darcs-hash:04daf0766ee72d8b51ea827d1a2ea2ccd842df0b

--- benchees/intel-mkl/doit32f_p4.c
+++ benchees/intel-mkl/doit32f_p4.c
@@ -1,4 +1,4 @@
 #define NAME "intel-mkl32-f-p4"
-#define RIGHT_PROCESSOR (DOUBLE_PRECISION || ((cpuid_edx(1) & (1 << 26)) && have_sse2()))
+#define RIGHT_PROCESSOR (((cpuid_edx(1) & (1 << 26)) && have_sse2()))
 
 #include "doitf.c"
