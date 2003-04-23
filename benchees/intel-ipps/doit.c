c5fc27dc5ca13e448fba8df4bf091988e3a7adb6
athena <none>
2003-04-23 13:21:17 -0400

    [project @ 2003-04-23 17:21:17 by athena]
    Added ipps (should probably do ippi too...)
    
    darcs-hash:f9b76e32fec7436ae262bc008b73576211796643

---
+++ benchees/intel-ipps/doit.c
@@ -0,0 +1,98 @@
+#include "bench-user.h"
+#include <math.h>
+#include <stdio.h>
+#include <ipps.h>
+
+static const char *mkvers(void)
+{
+    static char buf[1024];
+    const IppLibraryVersion *lib = ippsGetLibVersion();
+
+    sprintf(buf, "%s %s %d.%d.%d.%d", lib->Name, lib->Version,
+	    lib->major, lib->minor, lib->majorBuild, lib->build);
+    return buf;
+}
+
+BEGIN_BENCH_DOC
+BENCH_DOC("name", "intel-ipps")
+BENCH_DOC("package", "Intel Integrated Performance Primitives, Signal Processing (IPPS)")
+BENCH_DOCF("version", mkvers)
+BENCH_DOC("notes", "Using ``Perm'' format for real transforms.")
+END_BENCH_DOC
+
+#define CONCAT(prefix, name) prefix ## name
+
+#ifdef BENCHFFT_SINGLE
+#define MANGLEC(name) CONCAT(name, _32fc)
+#define MANGLE(name) CONCAT(name, _32f)
+#else
+#define MANGLEC(name) CONCAT(name, _64fc)
+#define MANGLE(name) CONCAT(name, _64f)
+#endif
+
+void copy_h2c(struct problem *p, bench_complex *out)
+{
+    copy_h2c_1d_packed(p, out, -1.0);
+}
+
+void copy_c2h(struct problem *p, bench_complex *in)
+{
+    copy_c2h_1d_packed(p, in, -1.0);
+}
+
+static void *thing;
+
+int can_do(struct problem *p)
+{
+    return (p->rank == 1 && problem_power_of_two(p, 0));
+}
+
+void setup(struct problem *p)
+{
+    if (p->kind == PROBLEM_COMPLEX) {
+	MANGLEC(ippsFFTInitAlloc_C)(&thing, 
+				    log_2(p->n[0]), 
+				    IPP_FFT_NODIV_BY_ANY,
+				    ippAlgHintAccurate);
+    } else {
+	MANGLE(ippsFFTInitAlloc_R)(&thing, 
+				   log_2(p->n[0]), 
+				   IPP_FFT_NODIV_BY_ANY,
+				   ippAlgHintAccurate);
+    }
+}
+
+void doit(int iter, struct problem *p)
+{
+    int i;
+
+    if (p->kind == PROBLEM_COMPLEX) {
+	if (p->sign == -1)
+	    for (i = 0; i < iter; ++i) {
+		MANGLEC(ippsFFTFwd_CToC)(p->in, p->out, thing, 0);
+	    }
+	else 
+	    for (i = 0; i < iter; ++i) {
+		MANGLEC(ippsFFTInv_CToC)(p->in, p->out, thing, 0);
+	    }
+    } else {
+	if (p->sign == -1)
+	    for (i = 0; i < iter; ++i) {
+		MANGLE(ippsFFTFwd_RToPerm)(p->in, p->out, thing, 0);
+	    }
+	else 
+	    for (i = 0; i < iter; ++i) {
+		MANGLE(ippsFFTInv_PermToR)(p->in, p->out, thing, 0);
+	    }
+    }
+}
+
+void done(struct problem *p)
+{
+    if (p->kind == PROBLEM_COMPLEX) {
+	MANGLEC(ippsFFTFree_C)(thing);
+    } else {
+	MANGLE(ippsFFTFree_R)(thing);
+    }
+    thing = 0;
+}
