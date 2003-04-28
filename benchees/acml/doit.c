91b08248409816ddafd99c46c9381064f3bea165
athena <none>
2003-04-27 20:01:41 -0400

    [project @ 2003-04-28 00:01:41 by athena]
    Added AMD's math library (still beta, not for public consumption)
    
    darcs-hash:da984127c19ba6725cdd18ccd3430ceb7f83720d

---
+++ benchees/acml/doit.c
@@ -0,0 +1,137 @@
+/* this program is in the public domain */
+
+#include "bench-user.h"
+#include <math.h>
+
+BEGIN_BENCH_DOC
+BENCH_DOC("name", "acml")
+END_BENCH_DOC
+
+
+int can_do(struct problem *p)
+{
+     switch (p->kind) {
+	 case PROBLEM_COMPLEX:
+	      return (p->rank >= 1 && p->rank <= 3 && problem_in_place(p));
+	 case PROBLEM_REAL:
+	      return (p->rank == 1 && problem_in_place(p));
+     }
+     return 0;
+}
+
+static void *WORK;
+
+#ifdef BENCHFFT_SINGLE
+#define CFFT1D cfft1d_
+#define CFFT2D cfft2d_
+#define CFFT3D cfft3d_
+#define SCFFT scfft_
+#define CSFFT csfft_
+#else
+#define CFFT1D zfft1d_
+#define CFFT2D zfft2d_
+#define CFFT3D zfft3d_
+#define SCFFT dzfft_
+#define CSFFT zdfft_
+#endif
+
+
+/* Undo normalization: */
+void after_problem_ccopy_to(struct problem *p, bench_complex *out)
+{
+     bench_complex x;
+     c_re(x) = sqrt(p->size);
+     c_im(x) = 0;
+     
+     cascale(out, p->size, x);
+}
+
+void copy_h2c(struct problem *p, bench_complex *out)
+{
+     copy_h2c_1d_halfcomplex(p, out, -1.0);
+}
+
+void copy_c2h(struct problem *p, bench_complex *in)
+{
+     copy_c2h_1d_halfcomplex(p, in, +1.0);
+     ascale(p->in, p->size, 2.0);
+}
+
+void setup(struct problem *p)
+{
+     int mode = 0;
+     int info;
+ 
+     BENCH_ASSERT(can_do(p));
+ 
+     if (p->kind == PROBLEM_COMPLEX) {
+	  switch (p->rank) {
+	      case 1:
+		   WORK = bench_malloc((2*p->n[0] + 100) *
+				       sizeof(bench_complex));
+		   CFFT1D(&mode, p->n, p->in, WORK, &info);
+		   break;
+	      case 2:
+		   WORK = bench_malloc((p->n[0] * p->n[1] 
+					+ 2 * p->n[0] 
+					+ 2 * p->n[1])
+				       * sizeof(bench_complex));
+		   break;
+	      case 3:
+		   WORK = bench_malloc((p->n[0] * p->n[1] * p->n[2]
+					+ 2 * p->n[0] 
+					+ 2 * p->n[1]
+					+ 2 * p->n[2])
+				       * sizeof(bench_complex));
+		   break;
+	  }
+     } else {
+	  WORK = bench_malloc((2*p->n[0] + 100) * sizeof(bench_complex));
+	  if (p->sign == -1) {
+	       SCFFT(&mode, p->n, p->in, WORK, &info);
+	  } else {
+	       CSFFT(&mode, p->n, p->in, WORK, &info);
+	  }
+     }
+}
+
+void doit(int iter, struct problem *p)
+{
+     int i;
+     void *in = p->in;
+     int sign = p->sign;
+     int info;
+
+     if (p->kind == PROBLEM_COMPLEX) {
+	  switch (p->rank) {
+	      case 1:
+		   for (i = 0; i < iter; ++i) 
+			CFFT1D(&sign, p->n, p->in, WORK, &info);
+		   break;
+	      case 2:
+		   for (i = 0; i < iter; ++i) 
+			CFFT2D(&sign, p->n + 1, p->n, p->in, WORK, &info);
+		   break;
+	      case 3:
+		   for (i = 0; i < iter; ++i) 
+			CFFT3D(&sign, p->n + 2, p->n + 1, p->n, 
+			       p->in, WORK, &info);
+		   break;
+	  }
+     } else {
+	  if (sign == -1) {
+	       sign = 1;
+	       for (i = 0; i < iter; ++i) 
+		    SCFFT(&sign, p->n, p->in, WORK, &info);
+	  } else {
+	       for (i = 0; i < iter; ++i) 
+		    CSFFT(&sign, p->n, p->in, WORK, &info);
+	  }
+     }
+}
+
+void done(struct problem *p)
+{
+     UNUSED(p);
+     bench_free(WORK);
+}
