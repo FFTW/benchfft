5eaaf467d20f7836672848c5d57750b97ce31f69
stevenj <none>
2003-04-29 18:37:58 -0400

    [project @ 2003-04-29 22:37:58 by stevenj]
    updated to latest FXT
    
    darcs-hash:82a144f9258be88f1b3491e58bc1e253d0941450

--- benchees/fxt/doit.cc
+++ benchees/fxt/doit.cc
@@ -8,12 +8,12 @@
 BEGIN_BENCH_DOC
 BENCH_DOC("name", NAME)
 BENCH_DOC("package", "FXT")
-BENCH_DOC("version", "26-August-2002")
+BENCH_DOC("version", "26-March-2003")
 BENCH_DOC("author", "J&ouml;rg Arndt")
-BENCH_DOC("year", "2002")
+BENCH_DOC("year", "2003")
 BENCH_DOC("email", "arndt@jjj.de")
 BENCH_DOC("url", "http://www.jjj.de/fxt/")
-BENCH_DOC("url-was-valid-on", "Sun Jul 15 00:02:30 EDT 2001")
+BENCH_DOC("url-was-valid-on", "Tue Apr 29 18:19:29 EDT 2003")
 BENCH_DOC("copyright", 
 "Copyright (c) 2001 Joerg Arndt\n"
 "\n"
--- benchees/fxt/doit_dit.cc
+++ benchees/fxt/doit_dit.cc
@@ -1,5 +1,5 @@
 #define NAME "fxt-dit"
 #define NOTES "Radix-4 DIT algorithm, complex datatype.  Backwards transform has explicit conjugation pass."
-#define DOIT_FFT dit4_fft(x, m, is)
+#define DOIT_FFT fft_dit4(x, m, is)
 
 #include "doit.cc"
