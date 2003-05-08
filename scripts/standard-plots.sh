f9cca043212ed656e6bc9593931e29531de691bb
stevenj <none>
2003-05-08 19:57:16 -0400

    [project @ 2003-05-08 23:57:16 by stevenj]
    double then single precision
    
    darcs-hash:b9b2e3421a27a57f522dc42e9f7d856aba7fcbd6

--- scripts/standard-plots.sh
+++ scripts/standard-plots.sh
@@ -10,7 +10,20 @@ data=$1
 dname=`basename $data .speed`
 
 for rank in 1 2 3; do
-    for problem in dcxx drxx scxx scxx srxx; do
+    for problem in dcxx drxx; do
+	for p2 in p2 np2; do
+	    echo "${dname}.${rank}d.${problem}.${p2}.ps"
+	    pat=`echo $problem | sed 's/xy/[io][fb]/g'`
+	    (sh $sd/plot-title.sh $problem $rank $p2;
+		egrep "$pat" $data | perl $sd/grep-rank.pl $rank |
+		perl $sd/grep-${p2}.pl | perl $sd/grace-plot.pl --no-dups) |
+		gracebat -pipe -printfile ${dname}.${rank}d.${problem}.${p2}.ps
+	done
+    done
+done
+
+for rank in 1 2 3; do
+    for problem in scxx srxx; do
 	for p2 in p2 np2; do
 	    echo "${dname}.${rank}d.${problem}.${p2}.ps"
 	    pat=`echo $problem | sed 's/xy/[io][fb]/g'`
