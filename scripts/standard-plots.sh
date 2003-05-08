8292480e21cf8b52b4164996b05c16a9d6e2d280
stevenj <none>
2003-05-08 19:35:45 -0400

    [project @ 2003-05-08 23:35:45 by stevenj]
    should work if run from elsewhere
    
    darcs-hash:b3fca5ab7b89be851e592ff64452f6eb55bf49b6

--- scripts/standard-plots.sh
+++ scripts/standard-plots.sh
@@ -1,16 +1,22 @@
 #! /bin/sh
 
+sd=`dirname $0`
+case $sd in
+    /*) ;;
+    *) sd=`pwd`/$sd ;;
+esac
+
 data=$1
 dname=`basename $data .speed`
 
 for rank in 1 2 3; do
     for problem in dcxx drxx scxx scxx srxx; do
 	for p2 in p2 np2; do
-	    echo "outputting ${dname}.${rank}d.${problem}.${p2}.ps"
+	    echo "${dname}.${rank}d.${problem}.${p2}.ps"
 	    pat=`echo $problem | sed 's/xy/[io][fb]/g'`
-	    (sh plot-title.sh $problem $rank $p2;
-		egrep "$pat" $data | perl grep-rank.pl $rank |
-		perl grep-${p2}.pl | perl grace-plot.pl --no-dups) |
+	    (sh $sd/plot-title.sh $problem $rank $p2;
+		egrep "$pat" $data | perl $sd/grep-rank.pl $rank |
+		perl $sd/grep-${p2}.pl | perl $sd/grace-plot.pl --no-dups) |
 		gracebat -pipe -printfile ${dname}.${rank}d.${problem}.${p2}.ps
 	done
     done
