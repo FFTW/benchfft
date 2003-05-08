b63ebf68deedfced34b983fe9ce89188dfa7505f
stevenj <none>
2003-05-08 19:57:33 -0400

    [project @ 2003-05-08 23:57:33 by stevenj]
    whoops
    
    darcs-hash:74470edb6d6805d8426274d0f49d88fb8b495994

--- scripts/standard-plots.sh
+++ scripts/standard-plots.sh
@@ -13,7 +13,7 @@ for rank in 1 2 3; do
     for problem in dcxx drxx; do
 	for p2 in p2 np2; do
 	    echo "${dname}.${rank}d.${problem}.${p2}.ps"
-	    pat=`echo $problem | sed 's/xy/[io][fb]/g'`
+	    pat=`echo $problem | sed 's/xx/[io][fb]/g'`
 	    (sh $sd/plot-title.sh $problem $rank $p2;
 		egrep "$pat" $data | perl $sd/grep-rank.pl $rank |
 		perl $sd/grep-${p2}.pl | perl $sd/grace-plot.pl --no-dups) |
@@ -26,7 +26,7 @@ for rank in 1 2 3; do
     for problem in scxx srxx; do
 	for p2 in p2 np2; do
 	    echo "${dname}.${rank}d.${problem}.${p2}.ps"
-	    pat=`echo $problem | sed 's/xy/[io][fb]/g'`
+	    pat=`echo $problem | sed 's/xx/[io][fb]/g'`
 	    (sh $sd/plot-title.sh $problem $rank $p2;
 		egrep "$pat" $data | perl $sd/grep-rank.pl $rank |
 		perl $sd/grep-${p2}.pl | perl $sd/grace-plot.pl --no-dups) |
