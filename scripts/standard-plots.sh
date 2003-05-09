0153d14acf5f6e2009085db67e370ab0ac324d99
stevenj <none>
2003-05-09 15:56:21 -0400

    [project @ 2003-05-09 19:56:21 by stevenj]
    produce accuracy plots
    
    darcs-hash:db59e0d16aa2f13ee0374afcc95e1efed0a65e03

--- scripts/standard-plots.sh
+++ scripts/standard-plots.sh
@@ -7,9 +7,14 @@ case $sd in
 esac
 
 data=$1
-dname=`basename $data .speed`
+case $data in
+    *.speed) type=speed; dname=`basename $data .speed` ;;
+    *.accuracy) type=accuracy; dname=`basename $data .accuracy` ;;
+esac
 
 outoforder='djbfft|athfft|pfftw'
+
+if test $type = speed; then
 
 for rank in 1 2 3; do
     for problem in dcxx drxx; do
@@ -38,3 +43,28 @@ for rank in 1 2 3; do
 	done
     done
 done
+
+else # $type = accuracy
+
+rank=1
+
+for problem in dcxx drxx scxx srxx; do
+    pat=`echo $problem | sed 's/xx/[io][fb]/g'`
+    (sh $sd/plot-title.sh $problem $rank;
+    egrep "$pat" $data | egrep -v "$outoforder" |
+	perl $sd/grep-rank.pl $rank |
+	perl $sd/grace-plot.pl --no-dups --accuracy) |
+	gracebat -pipe -printfile ${dname}.${rank}d.${problem}.ps
+    echo "${dname}.${rank}d.${problem}.ps"
+
+    pat=`echo $problem | sed 's/xx/[io][fb]/g'`
+    (sh $sd/plot-title.sh $problem $rank;
+     echo "@subtitle \"enlargement of most accurate FFTs only\"";
+     egrep "$pat" $data | egrep -v "$outoforder" |
+	perl $sd/grep-rank.pl $rank |
+	perl $sd/grace-plot.pl --no-dups --accuracy --accurate-only) |
+	gracebat -pipe -printfile ${dname}.${rank}d.${problem}.acc.ps
+    echo "${dname}.${rank}d.${problem}.acc.ps"
+done
+
+fi
