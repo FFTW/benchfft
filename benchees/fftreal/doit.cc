fccba627bfd91ef406d77a5ea0f1cfaeb29cd4fc
stevenj <none>
2003-08-15 17:29:31 -0400

    [project @ 2003-08-15 21:29:31 by stevenj]
    whoops
    
    darcs-hash:d3b85536f272cb10b0cc5ad6618877fc064bb233

--- benchees/fftreal/doit.cc
+++ benchees/fftreal/doit.cc
@@ -11,11 +11,7 @@ BENCH_DOC("version", "1.02")
 BENCH_DOC("author", "Laurent de Soras")
 BENCH_DOC("email", "ldesoras@club-internet.fr")
 BENCH_DOC("copyright",
-	  "Source code may be freely used for any purpose, including commercial
-applications. Programs must display in their 'About' dialog-box (or
-documentation) a text telling they use these routines by Laurent de Soras.
-Modified source code can be distributed, but modifications must be clearly
-indicated.")
+	  "Source code may be freely used for any purpose, including commercial applications. Programs must display in their 'About' dialog-box (or documentation) a text telling they use these routines by Laurent de Soras. Modified source code can be distributed, but modifications must be clearly indicated.")
 BENCH_DOC("language", "C++")
 BENCH_DOC("notes", "Code is in single precision, but double precision version is created as documented by the author, by changing the flt_t typedef in the FFTReal.h header file")
 BENCH_DOC("notes", "According to the README: There is no official web site where to get these files. If you have them, that's all to the good for you.")
