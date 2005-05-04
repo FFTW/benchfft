50ab9c23c41397421142bd7d20d80ef1908ae90a
stevenj <none>
2005-05-03 22:52:54 -0400

    [project @ 2005-05-04 02:52:54 by stevenj]
    added (NR-like) FFT from SciMark benchmark suite
    
    darcs-hash:b1e98ddc519f1a4b11997588de5d01396a425979

---
+++ benchees/scimark2c/doit.c
@@ -0,0 +1,60 @@
+/* this program is in the public domain */
+
+#include "bench-user.h"
+
+#include "FFT.h"
+
+BEGIN_BENCH_DOC
+
+BENCH_DOC("name", "scimark2c")
+BENCH_DOC("package", "SciMark")
+BENCH_DOC("version", "2.0")
+BENCH_DOC("language", "C")
+BENCH_DOC("year", "2000")
+BENCH_DOC("author", "Roldan Pozo")
+BENCH_DOC("email", "pozo@nist.gov")
+BENCH_DOC("author", "Bruce Miller")
+BENCH_DOC("email", "bruce.miller@nist.gov")
+BENCH_DOC("url", "http://math.nist.gov/scimark2/")
+BENCH_DOC("url-was-valid-on", "Tue May  3 22:33:43 EDT 2005")
+BENCH_DOC("notes", "The backwards transform is scaled.")
+
+END_BENCH_DOC
+
+int can_do(struct problem *p)
+{
+     return (DOUBLE_PRECISION &&
+	     p->rank == 1 &&
+	     p->kind == PROBLEM_COMPLEX &&
+	     problem_power_of_two(p, 1)
+	  );
+}
+
+void setup(struct problem *p)
+{
+     BENCH_ASSERT(can_do(p));
+}
+
+void done(struct problem *p)
+{
+     UNUSED(p);
+}
+
+void after_problem_ccopy_to(struct problem *p, bench_complex *out)
+{
+     unnormalize(p, out, +1);
+}
+
+void doit(int iter, struct problem *p)
+{
+     double *data = (double *) p->in;
+     int N = p->size * 2;
+     int i;
+
+     if (p->sign < 0)
+	  for (i = 0; i < iter; ++i)
+	       FFT_transform(N, data);
+     else
+	  for (i = 0; i < iter; ++i)
+	       FFT_inverse(N, data);
+}
