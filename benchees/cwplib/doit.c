5fe593b5c5b04f004b3472466b9f5dfac6d3a8f6
stevenj <none>
2006-01-24 19:17:50 -0500

    [project @ 2006-01-25 00:17:50 by stevenj]
    update to latest version (the only substantial difference is inclusion of double-precision routines) ... note that the entire CWP package is now 40MB, so I only included the relevant three files
    
    darcs-hash:4344f73b89d9e3202e4ee32548caf866b9e95b05

--- benchees/cwplib/doit.c
+++ benchees/cwplib/doit.c
@@ -6,21 +6,61 @@
 BEGIN_BENCH_DOC
 BENCH_DOC("name", "cwplib")
 BENCH_DOC("author", "Dave Hale")
-BENCH_DOC("year", "1989")
+BENCH_DOC("year", "1998")
+BENCH_DOC("version", "Release 39")
 BENCH_DOC("language", "C")
-BENCH_DOC("url", "http://risc2.numis.nwu.edu/ftp/pub/transforms/cwplib.tar.gz")
-BENCH_DOC("url-was-valid-on", "Sat Aug 31 16:39:12 EDT 2002")
-BENCH_DOC("copyright", "Copyright (c) Colorado School of Mines, 1995.")
+BENCH_DOC("url", "http://www.cwp.mines.edu/cwpcodes/")
+BENCH_DOC("url-was-valid-on", "Tue Jan 24 18:54:17 EST 2006")
+BENCH_DOC("copyright", "Copyright (c) 2005, Colorado School of Mines, all rights reserved.\n"
+"\n"
+"Warranty Disclaimer:\n"
+"NO GUARANTEES, OR WARRANTIES, EITHER EXPRESS OR IMPLIED, ARE PROVIDED BY \n"
+"CWP, CSM, ANY EMPLOYEE OR MEMBER OF THE AFORESAID ORGANIZATIONS, OR BY \n"
+"ANY CONTRIBUTOR TO THIS SOFTWARE PACKAGE, REGARDING THE ACCURACY, SAFETY, \n"
+"FITNESS FOR A PARTICULAR PURPOSE, OR ANY OTHER QUALITY OR CHARACTERISTIC \n"
+"OF THIS SOFTWARE, OR ANY DERIVATIVE SOFTWARE.\n"
+"\n"
+"Export Restriction Disclaimer:\n"
+"We believe that CWP/SU: Seismic Un*x is a low technology product that does\n"
+"not appear on the Department of Commerce CCL list of restricted exports.\n"
+"Accordingly, we believe that our product meets the qualifications of\n"
+"an ECCN (export control classification number) of EAR99 and we believe\n"
+"it fits the qualifications of NRR (no restrictions required), and\n"
+"is thus not subject to export restrictions of any variety.\n"
+"\n"
+"Limited License:\n"
+"The CWP/SU Seismic Un*x package (SU) is not public domain software, but \n"
+"it is available free under the following terms and conditions:\n"
+"\n"
+"1. Permission to use, copy, and modify this software for any purpose without\n"
+"fee and within the guidelines set forth below is hereby granted, provided \n"
+"that the above copyright notice, the warranty disclaimer, and this \n"
+"permission notice appear in all copies, and the name of the \n"
+"Colorado School of Mines (CSM) not be used\n"
+"in advertising or publicity pertaining to this software without the \n"
+"specific, written permission of CSM.\n"
+"\n"
+"2. The simple repackaging and selling of the SU package as is, as a \n"
+"commercial software product, is expressly forbidden without the prior \n"
+"written permission of CSM.  Any approved repackaging arrangement will \n"
+"carry the following restriction: only a modest profit over reproduction \n"
+"costs may be realized by the reproducer.\n")
+BENCH_DOC("notes", "Revised by Baoniu Han to handle double precision. 12/14/98")
+BENCH_DOC("notes", "Benchmark uses the files pfafft.c, dpfafft.c, and cwp.h, which are included as a part of the CWP/SU Seismic Un*x package.")
 END_BENCH_DOC
 
 #include "cwp.h"
 
+/* cwp double-precision routines have _d suffix */
+#ifdef BENCHFFT_SINGLE
+#  define F(x) x
+#else
+#  define F(x) x ## _d
+#endif
+
 int can_do(struct problem *p)
 {
      unsigned int i;
-
-     if (!SINGLE_PRECISION)
-	  return 0;
 
      if (p->kind == PROBLEM_COMPLEX) {
 	  /* 
@@ -37,7 +77,7 @@ int can_do(struct problem *p)
 	       return 0;
 
 	  for (i = 0; i < p->rank; ++i) {
-	       if ((unsigned int)npfa(p->n[i]) != p->n[i])
+	       if ((unsigned int) F(npfa)(p->n[i]) != p->n[i])
 		    return 0;
 	  } 
      } else {
@@ -47,7 +87,7 @@ int can_do(struct problem *p)
 
 	  /* can be either in place or out of place */
 
-	  if ((unsigned int)npfar(p->n[0]) != p->n[0])
+	  if ((unsigned int) F(npfar)(p->n[0]) != p->n[0])
 	       return 0;
      }
 
@@ -82,7 +122,7 @@ void doit(int iter, struct problem *p)
 	      {
 		   int n = p->n[0];
 		   for (i = 0; i < iter; ++i) 
-			pfacc(sign, n, in);
+			F(pfacc)(sign, n, in);
 		   break;
 	      }
 	      case 2:
@@ -90,8 +130,8 @@ void doit(int iter, struct problem *p)
 		   int n0 = p->n[0];
 		   int n1 = p->n[1];
 		   for (i = 0; i < iter; ++i) { 
-			pfa2cc(sign, 1, n1, n0, in);
-			pfa2cc(sign, 2, n1, n0, in);
+			F(pfa2cc)(sign, 1, n1, n0, in);
+			F(pfa2cc)(sign, 2, n1, n0, in);
 		   }
 		   break;
 	      }
@@ -107,10 +147,10 @@ void doit(int iter, struct problem *p)
 	  if (sign == -1) {
 	       /* real->complex */
 	       for (i = 0; i < iter; ++i) 
-		    pfarc(sign, n, in, out);
+		    F(pfarc)(sign, n, in, out);
 	  } else {
 	       for (i = 0; i < iter; ++i) 
-		    pfacr(sign, n, in, out);
+		    F(pfacr)(sign, n, in, out);
 	  }
      }
 }
