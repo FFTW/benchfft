091b828150555ea836713411be62714556763437
stevenj <none>
2006-02-03 01:44:38 -0500

    [project @ 2006-02-03 06:44:38 by stevenj]
    double-quotes in strings confuse some of our scripts
    
    darcs-hash:2d8145e36c876a3fa13693a4d79e195081eb2e0b

--- benchees/fxt/doit_gray.cc
+++ benchees/fxt/doit_gray.cc
@@ -1,5 +1,5 @@
 #define NAME "fxt-gray"
-#define NOTES "Radix-2 \"Gray-code\" algorithm, interleaved real/imag format."
+#define NOTES "Radix-2 'Gray-code' algorithm, interleaved real/imag format."
 
 #define DOIT_FFT gfft(x, m, is)
 
