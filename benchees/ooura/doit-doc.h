29fe25f34299ced584095987dd911d00f444ba25
stevenj <stevenj@fftw.org>
2008-10-25 16:13:28 -0400

    latest version of Ooura code (no significant changes, new URL)
    darcs-hash:e8d2ed0edd10c44d0b226499b3587900cb13ac7c

--- benchees/ooura/doit-doc.h
+++ benchees/ooura/doit-doc.h
@@ -2,11 +2,11 @@ BEGIN_BENCH_DOC
 BENCH_DOC("name", NAME)
 BENCH_DOC("package", "ooura")
 BENCH_DOC("author", "Takuya Ooura")
-BENCH_DOC("version", "2001/11/22")
-BENCH_DOC("year", "2001")
-BENCH_DOC("email", "ooura@mmm.t.u-tokyo.ac.jp")
-BENCH_DOC("url", "http://momonga.t.u-tokyo.ac.jp/~ooura/fft.html")
-BENCH_DOC("url-was-valid-on", "Sat Feb 23 09:00:34 EST 2002")
+BENCH_DOC("version", "2006/12/28")
+BENCH_DOC("year", "2006")
+BENCH_DOC("email", "ooura@kurims.kyoto-u.ac.jp")
+BENCH_DOC("url", "http://www.kurims.kyoto-u.ac.jp/~ooura/")
+BENCH_DOC("url-was-valid-on", "Sat Oct 25 16:12:11 EDT 2008")
 BENCH_DOC("copyright", 
 	  "Copyright(C) 1996-2001 Takuya OOURA\n"
 	  "You may use, copy, modify this code for any purpose and\n"
